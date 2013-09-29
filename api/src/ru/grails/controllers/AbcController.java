9d1911ec417e76a0b2b09f17406edc367dbf7c44
fedor.belov <fedor.belov@mail.ru>
2013-09-29 14:45:49 +0400

    gradle build

---
+++ api/src/ru/grails/controllers/AbcController.java
@@ -0,0 +1,8 @@
+/*
+ * AbcController
+ * Copyright (c) 2012 Cybervision. All rights reserved.
+ */
+package ru.grails.controllers;
+
+public class AbcController {
+}
---
+++ subProject/src/ru/grails/services/SubProjectService.java
@@ -0,0 +1,8 @@
+/*
+ * SubProjectService
+ * Copyright (c) 2012 Cybervision. All rights reserved.
+ */
+package ru.grails.services;
+
+public class SubProjectService {
+}
